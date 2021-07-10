4154d402fdbce0ba9c917f706f86b7dbbd5f8b4e
kwas <kwas@kwas-MS-7641>
2021-07-10 10:29:09 +0300

    Refactor

---
+++ Task_1/src/com/epam/training/den/Euclid.java
@@ -0,0 +1,54 @@
+package com.epam.training.den;
+
+public class Euclid {
+	public static long getDen(long[] ar) {
+		if (ar.length < 2)
+			return 0;
+		long a = ar[0];
+		long b = ar[1];
+		try {
+			while ((a != 0) && (b != 0)) {
+				if (a > b) {
+					a = a % b;
+				} else {
+					b = b % a;
+				}
+			}
+			for (int i = 2; i < ar.length; i++) {
+				b = a + b;
+				a = ar[i];
+				while ((a != 0) && (b != 0)) {
+					if (a > b) {
+						a = a % b;
+					} else {
+						b = b % a;
+					}
+				}
+			}
+		} catch (ArithmeticException e) {
+			System.out.println("zero");
+		}
+		return b + a;
+	}
+
+	public static long get(long a, long b) {
+		long bu = 0;
+		long x = a < b ? a : b;
+		long y = a < b ? b : a;
+		boolean w = true;
+		try {
+			while (w) {
+				bu = y % x;
+				if (bu != 0) {
+					y = x;
+					x = bu;
+				} else {
+					w = false;
+				}
+			}
+		} catch (ArithmeticException e) {
+			System.out.println("zero");
+		}
+		return x;
+	}
+}
--- Task_1/src/com/epam/training/den/Main.java
+++ Task_1/src/com/epam/training/den/Main.java
@@ -1,8 +1,11 @@
 package com.epam.training.den;
 
+//Common divisor and
+//the least common multiple
 public class Main {
-
 	public static void main(String args[]) {
-
+		Logic l = new Logic();
+		l.start();
 	}
+
 }
